df31db974188551d856e5fdd5318fe1e33bea83e
Deepak <deepakkotamsetti16@gmail.com>
2025-08-11 17:08:43 +0530

    Make render-build.sh executable and update configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,7 +84,6 @@ Rails.application.configure do
     authentication:       "plain",
     enable_starttls_auto: true
   }
-  
   config.action_mailer.default_url_options = { host: ENV["APP_HOST"], protocol: "https" }
   
 end
